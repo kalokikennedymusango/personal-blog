553a1a4e5916bda2c5707d5b28ce473d23cf31b8
kalokikennedymusango <astrohfamous20@yahoo.com>
2021-03-01 15:12:16 +0300

    mycommit

--- astroh/settings.py
+++ astroh/settings.py
@@ -29,10 +29,9 @@ ALLOWED_HOSTS = ('*')
 
 
 # Application definition
-        
+
 INSTALLED_APPS = [
     'ken',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
