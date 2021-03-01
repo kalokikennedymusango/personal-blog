add05b5fa460623d60ea2a81771c3a565e7160cb
kalokikennedymusango <astrohfamous20@yahoo.com>
2021-03-01 12:34:03 +0300

    mycommit

--- astroh/settings.py
+++ astroh/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!j$($szox6fbl)k3!mqb-s-jdia$dme7c*1b$*t4izs_x$&=yf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
